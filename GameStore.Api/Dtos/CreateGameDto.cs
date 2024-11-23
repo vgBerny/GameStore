55ad592cd3a70e9d2797afc02ae08ca5b667b679
WalterMelon~ <vgberny@gmail.com~>
2024-11-22 21:53:02 -0600

    CRUD finalizado

--- GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,8 +1,7 @@
 namespace GameStore.Api.Dtos;
 
-public record class GameDto(
-    int Id,
+public record class CreateGameDto(
     string Name,
     string Genre,
     decimal Price,
-    DateOnly ReseleDate);
+    DateOnly ReleaseDate);
--- GameStore.Api/Dtos/UpdateGameDto.cs
+++ GameStore.Api/Dtos/UpdateGameDto.cs
@@ -1,8 +1,8 @@
 namespace GameStore.Api.Dtos;
 
-public record class GameDto(
-    int Id,
+public record class UpdateGameDto(
     string Name,
     string Genre,
     decimal Price,
-    DateOnly ReseleDate);
+    DateOnly ReleaseDate);
+
